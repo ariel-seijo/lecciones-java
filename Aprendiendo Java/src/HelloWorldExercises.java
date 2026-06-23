455da7b17b8c84b5ec51753cdb9c4dce2432baa2
Ariel Seijo <hariel.seijo@gmail.com>
2026-06-23 19:26:32 -0300

    Se realizan ejercicios introductorios de Java

---
+++ Aprendiendo Java/src/HelloWorldExercises.java
@@ -0,0 +1,44 @@
+public class HelloWorldExercises {
+    public static void main(String[] args) {
+
+        // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+
+        /* System.out.println("Ariel Seijo"); */
+
+        // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
+
+        /* System.out.println("Hola\nMundo"); */
+
+        // 3. Añade un comentario sobre lo que hace cada línea del programa.
+        // no
+
+        // 4. Crea un comentario en varias líneas.
+
+        /* listo
+        papu */
+
+        // 5. Imprime tu edad, tu color favorito y tu ciudad.
+
+//        System.out.println("Edad: 23\nColor favorito: Negro\nCiudad: Buenos Aires");
+
+        // 6. Explora los diferentes System.XXX.println(); más allá de "out".
+
+        // 7. Utiliza varios println para imprimir una frase.
+
+/*        System.out.println("Hola");
+        System.out.println("mundo");*/
+
+        // 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos).
+
+/*        System.out.println("O  O");
+        System.out.println(" x  ");
+        System.out.println("____" );*/
+
+        // 9. Intenta ejecutar el programa sin el método main y observa el error.
+
+//        System.out.println("____" );
+
+        //10. Intenta cambiar el nombre del archivo a uno diferente del de la clase y compílalo. ¿Qué pasa?
+        System.out.println("Todos los ejercicios fueron realizados con exito, papu");
+    }
+}
