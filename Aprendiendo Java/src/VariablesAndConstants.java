093dc897ff3a4a8d70d87e7caaebb9e46b57e107
Ariel Seijo <hariel.seijo@gmail.com>
2026-06-23 19:50:25 -0300

    Práctica de constantes y variables

---
+++ Aprendiendo Java/src/VariablesAndConstants.java
@@ -0,0 +1,31 @@
+public class VariablesAndConstants {
+    public static void main(String[] args) {
+
+        // variables
+
+        String name = "Arielo";
+        String space = " ";
+        String surname = "Seijo";
+
+        System.out.println(name + space + surname);
+
+        // reasigno variable
+
+        name = "Arielovich";
+
+        System.out.println(name + space + surname);
+
+        // otra forma de crear variables sin declarar tipo de dato
+
+        var name2 = "Arielovich";
+
+        System.out.println(name2);
+
+        // constantes
+
+        final String email = "arielovich@email.com";
+
+        System.out.println(email);
+
+    }
+}
