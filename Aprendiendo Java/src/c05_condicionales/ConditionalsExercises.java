e931a25a69a4e85f2a95f4c9322a6a6619f9087a
Ariel Seijo <hariel.seijo@gmail.com>
2026-06-24 21:23:45 -0300

    Se resuelven ejercicios de condicionales

---
+++ Aprendiendo Java/src/c05_condicionales/ConditionalsExercises.java
@@ -0,0 +1,145 @@
+package c05_condicionales;
+
+public class ConditionalsExercises {
+
+    public static void main(String[] args) {
+
+        // 1. Establece la edad de un usuario y muestra si puede votar (mayor o igual a 18).
+
+//        int edad = 19;
+//
+//        if (edad >= 18) {
+//            System.out.println("Puede votar.");
+//        } else {
+//            System.out.println("No puede votar.");
+//        }
+
+        // 2. Declara dos números y muestra cuál es mayor, o si son iguales.
+
+//        int A = 10;
+//        int B = 10;
+//
+//        if (A > B) {
+//            System.out.println("A es mayor");
+//        } else if (A < B) {
+//            System.out.println("B es mayor");
+//        } else {
+//            System.out.println("A y B son iguales.");
+//        }
+
+        // 3. Dado un número, verifica si es positivo, negativo o cero.
+
+//        int num = 0;
+//
+//        if (num > 0) {
+//            System.out.println("El número es positivo");
+//        } else if (num < 0) {
+//            System.out.println("El número es negativo");
+//        } else {
+//            System.out.println("El número es igual a 0");
+//        }
+
+        // 4. Crea un programa que diga si un número es par o impar.
+
+//        int num = 10;
+//
+//        if (num % 2 == 0) {
+//            System.out.println("El número es par");
+//        } else {
+//            System.out.println("El número es impar");
+//        }
+
+        // 5. Verifica si un número está en el rango de 1 a 100.
+
+//        int num = 100;
+//
+//        if (num >= 1 && num <= 100) {
+//            System.out.println("El número está en el rango de 1 a 100");
+//        } else {
+//            System.out.println("El número NO está en el rango de 1 a 100");
+//        }
+
+        // 6. Declara una variable con el día de la semana (1-7) y muestra su nombre con switch.
+
+//        int dia = 7;
+//
+//        switch (dia) {
+//            case 1:
+//                System.out.println("El día es lunes");
+//                break;
+//            case 2:
+//                System.out.println("El día es martes");
+//                break;
+//            case 3:
+//                System.out.println("El día es miercoles");
+//                break;
+//            case 4:
+//                System.out.println("El día es jueves");
+//                break;
+//            case 5:
+//                System.out.println("El día es viernes");
+//                break;
+//            case 6:
+//                System.out.println("El día es sabado");
+//                break;
+//            case 7:
+//                System.out.println("El día es domingo");
+//        }
+
+        // 7. Simula un sistema de notas: muestra "Sobresaliente", "Aprobado" o "Suspenso" según la nota (0-100).
+
+//        int nota = 40;
+//
+//        if (nota < 40) {
+//            System.out.println("Suspenso");
+//        } else if (nota < 90 ) {
+//            System.out.println("Aprobado");
+//        } else {
+//            System.out.println("Sobresaliente");
+//        }
+
+        // 8. Escribe un programa que determine si puedes entrar al cine: debes tener al menos 15 años o ir acompañado.
+
+        int edad = 18;
+        boolean acompaniado = false;
+
+//        if (edad >= 18) {
+//            System.out.println("Puedes pasar");
+//        } else if (acompaniado) {
+//            System.out.println("Puedes pasar acompañado");
+//        } else {
+//            System.out.println("No puedes pasar. Debes ser mayor o ir acompañado.");
+//        }
+
+//        if (edad >= 18 || acompaniado) {
+//            System.out.println("Puedes pasar");
+//        } else {
+//            System.out.println("No puedes pasar");
+//        }
+
+        // 9. Crea un programa que diga si una letra es vocal o consonante.
+
+//        char letra = 'i';
+//
+//        if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+//            System.out.println("Es vocal");
+//        } else {
+//            System.out.println("Es consonante");
+//        }
+
+//        // 10. Usa tres variables a, b, c y muestra cuál es el mayor de las tres.
+//
+//        int a = 9;
+//        int b = 10;
+//        int c = 11;
+//
+//        if (a > b && a > c) {
+//            System.out.println("A es mayor");
+//        } else if (c > b) {
+//            System.out.println("C es mayor");
+//        } else {
+//            System.out.println("B es mayor");
+//        }
+
+    }
+}
