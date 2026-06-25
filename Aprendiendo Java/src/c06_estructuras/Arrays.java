38c94871b30fd22b3c5b34936a04c8f46f9c1b43
Ariel Seijo <hariel.seijo@gmail.com>
2026-06-25 12:20:22 -0300

    Repasando estructuras de datos: listas

--- Aprendiendo Java/src/c06_estructuras/Arrays.java
+++ Aprendiendo Java/src/c06_estructuras/Arrays.java
@@ -1,4 +1,38 @@
 package c06_estructuras;
 
 public class Arrays {
+
+    public static void main(String[] args) {
+
+        // DECLARACION CON TAMAÑO FIJO
+
+        String[] apellidos = new String[3];
+        int[] titulos = new int[3];
+
+        // DECLARACION CON VALORES
+
+        String[] nombres = new String[]{"Naruto", "Sasuke", "Sakura"};
+        int[] edades = new int[]{15, 16, 16};
+
+//         EN JAVA +10
+
+        var apellidos2 = new String[3];
+        var titulos2 = new int[3];
+        var nombres2 = new String[]{"Naruto", "Sasuke", "Sakura"};
+        var edades2 = new int[]{15, 16, 17};
+
+//         ACCEDER A UN VALOR
+
+        System.out.println(apellidos[0]); // null para arrays vacios
+        System.out.println(titulos[0]); // 0 para ints por defecto
+
+        System.out.println(nombres[1]); //Sasuke
+        System.out.println(edades[1]); //16
+
+//         MODIFICACION VALOR
+
+        nombres[1] = "Kimimaro";
+        System.out.println(nombres[1]); // Sasuke -> Kimimaro
+
+    }
 }
--- Aprendiendo Java/src/c06_estructuras/Lists.java
+++ Aprendiendo Java/src/c06_estructuras/Lists.java
@@ -1,4 +1,53 @@
 package c06_estructuras;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class Lists {
+    public static void main(String[] args) {
+
+        // DECLARACION Y CREACION
+
+        ArrayList<String> nombres = new ArrayList<>();
+        var edades = new ArrayList<Integer>();
+
+        // TAMAÑO
+
+        System.out.println(nombres.size()); // 0
+
+        // AÑADIR ELEMENTOS (AL FINAL)
+
+        nombres.add("Naruto");
+        nombres.add("Sasuke");
+        nombres.add("Sakura");
+
+        System.out.println(nombres);
+
+        // ACCEDER A ELEMENTO
+
+        System.out.println(nombres.get(1));
+        System.out.println(nombres.getLast());
+        System.out.println(nombres.getFirst());
+
+        // MODIFICAR
+
+        nombres.set(1, "Kimimaro");
+        System.out.println(nombres.get(1));
+
+        // ELIMINAR
+
+        nombres.remove(1); // borra kimimaro
+//        nombres.removeLast();
+//        nombres.removeFirst();
+
+        // BUSCAR ELEMENTO
+
+        System.out.println(nombres.contains("Kimimaro"));
+        System.out.println(nombres.contains("Naruto"));
+
+        // LIMPIAR ARRAYLIST
+
+        nombres.clear();
+        System.out.println(nombres);
+    }
 }
