a1e095971d2adb6ee5624f458f8e3e1651aef4d4
Ariel Seijo <hariel.seijo@gmail.com>
2026-06-26 10:50:33 -0300

    Se resuelven ejercicios de estructuras de datos

---
+++ Aprendiendo Java/src/c06_estructuras/EjerciciosEstructuras.java
@@ -0,0 +1,74 @@
+package c06_estructuras;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class EjerciciosEstructuras {
+    public static void main(String[] args) {
+
+        // 1. Crea un Array con 5 valores e imprime su longitud.
+
+//        String[] nombres = new String[5];
+//        System.out.println(nombres.length);
+
+
+        // 2. Modifica uno de los valores del Array e imprime el valor del índice antes y después de modificarlo.
+
+//        System.out.println(nombres[1]); // null
+//        nombres[1] = "Hey";
+//        System.out.println(nombres[1]); // hey
+
+        // 3. Crea un ArrayList vacío.
+
+//        ArrayList<String> nombres = new ArrayList<>();
+
+        // 4. Añade 4 valores al ArrayList y elimina uno a continuación.
+
+//        nombres.add("Juancho");
+//        nombres.add("Juancho1");
+//        nombres.add("Juancho2");
+//        nombres.add("Juancho3");
+//        System.out.println(nombres);
+//        nombres.remove(1);
+//        System.out.println(nombres);
+
+        // 5. Crea un HashSet con 2 valores diferentes.
+
+//        HashSet<String> nombres = new HashSet<>();
+//        nombres.add("Arieloide");
+//        nombres.add("Arieloide2");
+//        System.out.println(nombres);
+
+        // 6. Añade un nuevo valor repetido y otro sin repetir al HashSet.
+
+//        nombres.add("Arieloide");
+//        nombres.add("Arieloide3");
+//        System.out.println(nombres);
+
+        // 7. Elimina uno de los elementos del HashSet.
+
+//        nombres.remove("Arieloide");
+//        System.out.println(nombres);
+
+        // 8. Crea un HashMap donde la clave sea un nombre y el valor el número de teléfono. Añade tres contactos.
+
+//        HashMap<String, Integer> contactos = new HashMap<>();
+//
+//        contactos.put("Arielo", 112507884);
+//        contactos.put("Martin", 111237214);
+//        contactos.put("Fernando", 134234212);
+//
+//        System.out.println(contactos);
+
+        // 9. Modifica uno de los contactos y elimina otro.
+
+//        contactos.replace("Arielo", 122313121);
+//        contactos.remove("Martin");
+//        System.out.println(contactos);
+
+
+    }
+}
