80edd907714420fb4f387020c01ca8e835967f3f
Ariel Seijo <hariel.seijo@gmail.com>
2026-07-17 20:20:15 -0300

    Creo un array de objetos y recorro cada uno imprimiendo el valor de sus getters

--- Aprendiendo Java/src/POO/usoEmpleado.java
+++ Aprendiendo Java/src/POO/usoEmpleado.java
@@ -3,10 +3,22 @@ import POO.Empleado;
 
 public class usoEmpleado {
     public static void main(String[] args) {
-        Empleado jorge = new Empleado("Jorge", "Varela", 23, 1800.50);
-        System.out.println(jorge.getNombre());
-        System.out.println(jorge.getApellido());
-        System.out.println(jorge.getEdad());
-        System.out.println(jorge.getSalario());
+        Empleado [] misEmpleados = new Empleado[3];
+
+        misEmpleados[0] = new Empleado("Jorge", "Varela", 21, 1800000.50);
+        misEmpleados[1] = new Empleado("Ariel", "Seijovich", 23, 1900000.30);
+        misEmpleados[2] = new Empleado("Pedrito", "Estevanez", 28, 1400000.50);
+
+/*        System.out.println("Nombre: " + misEmpleados[0].getNombre() +
+                "\nApellido: " + misEmpleados[0].getApellido() +
+                "\nEdad: " + misEmpleados[0].getEdad() +
+                "\nSalario: " + misEmpleados[0].getSalario());*/
+
+        for (Empleado empleado: misEmpleados) {
+            System.out.println("Nombre: " + empleado.getNombre() +
+                    "\nApellido: " + empleado.getApellido() +
+                    "\nEdad: " + empleado.getEdad() +
+                    "\nSalario: " + empleado.getSalario());
+        }
     }
 }
